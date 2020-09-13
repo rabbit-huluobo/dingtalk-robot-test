cbe0f53b8c6d5273d39748c3b259804add0d7303
胡萝卜 <zgjshahxw@sina.cn>
2020-09-13 23:12:26 +0800

    init

---
+++ src/main/java/com/woshale/dingtalkrobottest/DingtalkRobotTestApplication.java
@@ -0,0 +1,13 @@
+package com.woshale.dingtalkrobottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingtalkRobotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingtalkRobotTestApplication.class, args);
+    }
+
+}
